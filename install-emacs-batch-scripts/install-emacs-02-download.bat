437b5824a094da12f89064283a83482eff431c40
hubisan <hubisan@gmail.com>
2021-01-11 08:14:37 +0100

    some fixes, especially ubuntu version in batch scripts

--- install-emacs-batch-scripts/install-emacs-02-download.bat
+++ install-emacs-batch-scripts/install-emacs-02-download.bat
@@ -1,3 +1,3 @@
 @echo off
-wsl bash -c "cd ~ && wget https://ftp.gnu.org/pub/gnu/emacs/emacs-27.1.tar.gz && tar -xzvf emacs-27.1.tar.gz"
+wsl -d Ubuntu-20.04 bash -c "cd ~ && wget https://ftp.gnu.org/pub/gnu/emacs/emacs-27.1.tar.gz && tar -xzvf emacs-27.1.tar.gz"
 pause
--- install-emacs-batch-scripts/install-emacs-03-configure-install.bat
+++ install-emacs-batch-scripts/install-emacs-03-configure-install.bat
@@ -1,3 +1,3 @@
 @echo off
-wsl bash -c "cd ~/emacs-27.1 && ./configure --with-cairo && make && sudo make install && rm ~/emacs-27.1.tar.gz"
+wsl -d Ubuntu-20.04 bash -c "cd ~/emacs-27.1 && ./configure --with-cairo && make && sudo make install && rm ~/emacs-27.1.tar.gz"
 pause
