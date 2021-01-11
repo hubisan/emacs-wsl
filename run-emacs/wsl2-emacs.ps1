b74d62275c76e693cd62d29e51b1d6da61878415
hubisan <hubisan@gmail.com>
2021-01-12 00:34:28 +0100

    small fixes

--- run-emacs/wsl2-emacs.ps1
+++ run-emacs/wsl2-emacs.ps1
@@ -5,10 +5,3 @@ $wslip = wsl -d Ubuntu-20.04 bash -c 'ip route | awk ''/default via /'' | cut -d
 
 # Run Emacs
 wsl -d Ubuntu-20.04 bash -c "export DISPLAY=$wslip`:0.0 export LIBGL_ALWAYS_INDIRECT=1 && setsid emacs"
-
-# If you want to change the keyboard layout to US.
-# x11-xkb-utils needs to be installed (sudo apt install x11-xkb-utils).
-# wsl -d Ubuntu-20.04 bash -c "export DISPLAY=$wslip`:0.0 export LIBGL_ALWAYS_INDIRECT=1 && setxkbmap -layout us && setsid emacs"
-
-# If you want to use zsh.
-# wsl -d Ubuntu-20.04 zsh -c "export DISPLAY=%mywslip%:0.0 && export LIBGL_ALWAYS_INDIRECT=1 && setsid emacs"
