c191ac0ee72e24ea09a28a7e54ca69eab18aba45
hubisan <hubisan@gmail.com>
2021-04-05 18:25:25 +0200

    forgot to update to 27.2 in batch scripts

--- install-emacs/install-emacs-batch-02-download.bat
+++ install-emacs/install-emacs-batch-02-download.bat
@@ -1,7 +1,7 @@
 @echo off
 
-@REM 2. Download Emacs 27.1
+@REM 2. Download Emacs 27.2
 
-wsl -d Ubuntu-20.04 bash -c "cd ~ && wget https://ftp.gnu.org/pub/gnu/emacs/emacs-27.1.tar.gz && tar -xzvf emacs-27.1.tar.gz"
+wsl -d Ubuntu-20.04 bash -c "cd ~ && wget https://ftp.gnu.org/pub/gnu/emacs/emacs-27.2.tar.gz && tar -xzvf emacs-27.2.tar.gz"
 
 pause
--- install-emacs/install-emacs-batch-03-configure-install.bat
+++ install-emacs/install-emacs-batch-03-configure-install.bat
@@ -2,6 +2,6 @@
 
 @REM 3. Configure and install Emacs.
 
-wsl -d Ubuntu-20.04 bash -c "cd ~/emacs-27.1 && ./configure --with-cairo && make && sudo make install && rm ~/emacs-27.1.tar.gz"
+wsl -d Ubuntu-20.04 bash -c "cd ~/emacs-27.2 && ./configure --with-cairo && make && sudo make install && rm ~/emacs-27.2.tar.gz"
 
 pause
