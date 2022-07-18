1436afbf19951c4dd0d28322b6a0120547519bb6
hubisan <hubisan@gmail.com>
2022-07-18 23:10:12 +0200

    Uses Emacs version 28.1 now and native compilation.
    From the Changelog:
    
    - CHANGED Adapted instructions to use Emacs version 28.1 instead of 27.2.
    - CHANGED Using native compilation flag. Just remove it if you don't want it.
      It works very well on my setup (WSL 1 and Ubuntu 20.04).
    - ADDED New dependencies from the snap including those needed for native
      compilation.
    - REMOVED The batch scripts to install Emacs. Better do it in the terminal
      as there might be some packages missing or other errors.
    - ADDED Instructions to use the latest snapshot of Emacs.
    - ADDED Remark about WSLg when using Windows 11.
    - CHANGED Made the VcXsrv section a child of the run Emacs section.

--- install-emacs/install-emacs-batch-03-configure-install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-@REM 3. Configure and install Emacs.
-
-wsl -d Ubuntu-20.04 bash -c "cd ~/emacs-27.2 && ./configure --with-cairo && make && sudo make install && rm ~/emacs-27.2.tar.gz"
-
-pause
