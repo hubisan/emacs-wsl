95ff8b4373e3b50ad9fae935fe50e087e3dc1991
Daniel Hubmann <hubisan@gmail.com>
2023-12-14 17:37:56 +0100

    Remove WSL 1 Documentation (#60)
    * started with updating the documentation
    
    * adapted everzthing to wsl2
    
    * rendering on github not good enough yet
    
    * how this works now
    
    * test again
    
    * and again
    
    * test again
    
    * trying brbr and p
    
    * test
    
    * test again
    
    * test
    
    * test again
    
    * i give up
    
    * and again

--- run-emacs-wsl-2/wsl-2_2-firewall-rule.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-@rem Just run the PowerShell script.
-
-powershell -NoProfile -ExecutionPolicy Bypass -Command "& './wsl-2_2-firewall-rule.ps1'"
--- run-emacs-wsl-2/wsl-2_3-vcxsrv.bat
+++
@@ -1,2 +0,0 @@
-@rem Run VcXsrv
-start "" "${Env:programfiles}\VcXsrv\vcxsrv.exe" :0 -multiwindow -clipboard -wgl -ac
